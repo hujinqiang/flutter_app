f6612fc5fac28d3f4fe921f9ebeec0b2e9f150b4
hujinqiang <854024269@qq.com>
2019-03-07 14:51:25 +0800

    first Flutter app  list  pair name generator

--- lib/main_old.dart
+++ lib/main_old.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,61 +6,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWordState extends State<RandomWords>{
-
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions(){
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context,i){
-          if(i.isOdd) return Divider();
-
-          final index = i ~/ 2;
-          if(index >= _suggestions.length){
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair){
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Gennerator'),
-      ),
-      body: _buildSuggestions(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
-class RandomWords extends StatefulWidget{
-  @override
-  RandomWordState createState() => new RandomWordState();
-}
-
-
-
-
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
